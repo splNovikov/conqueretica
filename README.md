0fb6a934781fada9004bfb960d36fe964395f50a
Pavel Novikov <spl.novikov@gmail.com>
2021-07-29 14:06:39 +0300

    updated folders structure, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
  - Set up ESLint, Prettier auto-format in your IDE on save
+
+ #### todo:
+- reorganising folders structure, reformat current components
+- unit tests + test components
+- storybook
+- styled components?
+- storage - MobX?
+- api
---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <header className="app-header">
+      <img src={logo} className="app-logo" alt="logo" />
+    </header>
+  </div>
+);
+
+export default App;
