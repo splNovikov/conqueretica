f1fd11a4072f591de968a9609ea20e3f60814f1d
Pavel Novikov <spl.novikov@gmail.com>
2021-08-02 23:15:35 +0400

    test pre-push

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
 
-const App = () => (
-  <div className="app">
-    <header className="app-header">hi there</header>
-  </div>
-);
+const App = () => {
+  const x = (a: number) => (a > 2 ? 3 : 4);
+  return (
+    <div className="app">
+      <header className="app-header">hi there {x(2)}</header>
+    </div>
+  );
+};
 
 export default App;
