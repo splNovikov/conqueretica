1e2f305f3fb583606a2e947eb9c24a41c77e3174
Pavel Novikov <spl.novikov@gmail.com>
2021-08-02 23:37:32 +0400

    test pre-commit 3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 
 const App = () => {
-  const x = (a: number) => (a > 2 ? 3 : 5);
+  const x = (a: number) => (a > 2 ? 3 : 4);
   return (
     <div className="app">
       <header className="app-header">hi there {x(2)}</header>
