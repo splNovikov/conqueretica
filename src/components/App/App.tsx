7a485a630f919dc4b4ba1d32913ad96502ca338f
Pavel Novikov <spl.novikov@gmail.com>
2021-08-02 23:48:16 +0400

    check husky

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.scss';
 
-const App = () => {
-  const x = (a: number) => (a > 2 ? 3 : 4);
-  return (
-    <div className="app">
-      <header className="app-header">hi there {x(2)}</header>
-    </div>
-  );
-};
+const App = () => (
+  <div className="app">
+    <header className="app-header">hi there</header>
+  </div>
+);
 
 export default App;
