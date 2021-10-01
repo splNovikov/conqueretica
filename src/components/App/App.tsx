01625dcab2441d7ff26117cb268a68281b1d3c94
Pavel Novikov <spl.novikov@gmail.com>
2021-10-01 16:38:54 +0300

    removed button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,10 @@
 import React, { FC } from 'react';
 
 import './App.scss';
-import Button from '../Button';
 
 const App: FC = () => (
   <div className="app">
-    <header className="app-header">
-      hi there <Button aaa={1} htmlType="submit" />
-    </header>
+    <header className="app-header">hi there</header>
   </div>
 );
 
