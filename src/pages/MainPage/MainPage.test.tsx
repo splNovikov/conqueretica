d6484c8a29fd259d9f327af311e280ab38b807e0
Pavel Novikov <spl.novikov@gmail.com>
2021-11-01 16:20:45 +0300

    added todo - fix tests

--- src/pages/MainPage/MainPage.test.tsx
+++ src/pages/MainPage/MainPage.test.tsx
@@ -3,7 +3,9 @@ import { render } from '@testing-library/react';
 
 import MainPage from './MainPage';
 
+// todo: fix tests!!!
+
 test('MainPage is rendering', () => {
   const { container } = render(<MainPage />);
-  expect(container.firstChild).toHaveClass('app-wrapper');
+  expect(container.firstChild).toHaveClass('main-page-wrapper');
 });
