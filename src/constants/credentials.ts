554949c67a5aea584a61e5b248de82154c16822f
Pavel Novikov <spl.novikov@gmail.com>
2021-12-30 18:34:17 +0300

    rollback creds

--- src/constants/credentials.ts
+++ src/constants/credentials.ts
@@ -1,13 +1,6 @@
 // todo: this is a temporary solution - fix it by real auth service
-const HABITICA_ID = 'ff8da73e-0313-4d13-b238-446dc80058cb';
-const HABITICA_TOKEN = 'a614afbc-92db-4715-853b-da82b4462028';
-const FIREBASE = {
-  apiKey: 'AIzaSyB1cG9MwNMuIYCJ-9BkKSeDMQTR2I3SnrY',
-  authDomain: 'conqueretica.firebaseapp.com',
-  projectId: 'conqueretica',
-  storageBucket: 'conqueretica.appspot.com',
-  messagingSenderId: '334049998355',
-  appId: '1:334049998355:web:0c6ebf4a75f174f32431e6',
-};
+const HABITICA_ID = '';
+const HABITICA_TOKEN = '';
+const FIREBASE = {};
 
 export { HABITICA_ID, HABITICA_TOKEN, FIREBASE };
