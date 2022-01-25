23e6875b642fe89d68b9e177c375e700d8b5bc97
Pavel Novikov <spl.novikov@gmail.com>
2022-01-25 10:07:53 +0300

    updated google links. added Linky and display flex

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,50 @@
 import React, { FC } from 'react';
 import { User } from 'firebase/auth';
-
+// Interfaces
+import { ILink } from '../../interfaces';
+// Components
 import SignOut from '../SignOut';
 import Login from '../Login';
+import Linky from '../Linky';
+// Styles
+import './Header.scss';
+
+const googleLinks: ILink[] = [
+  {
+    id: '1',
+    href: 'https://docs.google.com/spreadsheets',
+    title: 'Sheets',
+  },
+  {
+    id: '2',
+    href: 'https://docs.google.com/document',
+    title: 'Docs',
+  },
+  {
+    id: '3',
+    href: 'https://docs.google.com/presentation',
+    title: 'Slides',
+  },
+];
 
 const Header: FC<{
   user: User | null | undefined;
 }> = ({ user }) => (
   <div className="header">
-    {user ? (
-      <div className="user-wrapper">
-        <span className="user-name">{user.displayName}</span>
-        <SignOut />
-      </div>
-    ) : (
-      <Login />
-    )}
     <div>
-      <a href="https://docs.google.com/spreadsheets">Sheets</a>
-      &nbsp;
-      <a href="https://docs.google.com/document">Docs</a>
+      {user ? (
+        <div className="user-wrapper">
+          <span className="user-name">{user.displayName}</span>
+          <SignOut />
+        </div>
+      ) : (
+        <Login />
+      )}
+    </div>
+    <div>
+      {googleLinks.map((l) => (
+        <Linky link={l} colored key={l.id} />
+      ))}
     </div>
   </div>
 );
