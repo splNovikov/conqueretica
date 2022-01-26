28d69552af9e8227a0926b147b0f4c7aa9c800cb
Pavel Novikov <spl.novikov@gmail.com>
2022-01-26 19:46:06 +0300

    deleted obsolete files

--- src/components/SendForm/SendForm.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-
-import SendForm from './SendForm';
-
-it('SendForm is rendering', () => {
-  const wrapper = shallow(<SendForm />);
-  expect(wrapper.hasClass('send-form')).toEqual(true);
-});
