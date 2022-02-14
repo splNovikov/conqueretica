d46f28560d7440772b2305a2df2cb35d0639d2dc
Pavel Novikov <spl.novikov@gmail.com>
2022-02-14 17:09:20 +0300

    fixed broken tests

--- src/components/Tabs/Tabs.test.tsx
+++ src/components/Tabs/Tabs.test.tsx
@@ -5,19 +5,29 @@ import Tabs from './Tabs';
 // Test Data
 import { tabs } from '../../__test_data__';
 
+const mockedFunctions = {
+  selectTabHandler: () => {},
+  deleteTabHandler: () => {},
+  tabsFormSubmitHandler: () => {},
+};
+
 describe('Tabs', () => {
   it('Tabs Component is rendering', () => {
-    shallow(<Tabs tabs={tabs} selectedTab={tabs[0]} />);
+    shallow(<Tabs tabs={tabs} selectedTab={tabs[0]} {...mockedFunctions} />);
   });
 
   it('Tabs Component render all tabs', () => {
-    const wrapper = shallow(<Tabs tabs={tabs} selectedTab={tabs[0]} />);
-    const tabsEl = wrapper.find('.tab');
+    const wrapper = shallow(
+      <Tabs tabs={tabs} selectedTab={tabs[0]} {...mockedFunctions} />,
+    );
+    const tabsEl = wrapper.find('Tab');
     expect(tabsEl.length).toBe(3);
   });
 
   it('Tabs Component Should select Selected Tab', () => {
-    const wrapper = mount(<Tabs tabs={tabs} selectedTab={tabs[0]} />);
+    const wrapper = mount(
+      <Tabs tabs={tabs} selectedTab={tabs[0]} {...mockedFunctions} />,
+    );
     const tabsEl = wrapper.find('.tab');
     expect(tabsEl.at(0).text()).toBe('test_tab_1[Selected]');
   });
