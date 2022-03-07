7662543a8c90680b4646a2e24287153206d4bd53
Pavel Novikov <spl.novikov@gmail.com>
2022-03-07 17:51:50 +0300

    added tests for Google Icon

---
+++ src/components/GoogleIcon/GoogleIcon.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+// Components
+import GoogleIcon from './GoogleIcon';
+
+describe('GoogleIcon', () => {
+  it('GoogleIcon is rendering', () => {
+    const wrapper = shallow(<GoogleIcon icon="sheets" />);
+    expect(wrapper.hasClass('google-icon')).toEqual(true);
+  });
+
+  it('GoogleIcon. Default size is set up', () => {
+    const wrapper = shallow(<GoogleIcon icon="sheets" />);
+    expect(wrapper.hasClass('normal')).toEqual(true);
+  });
+
+  it('GoogleIcon. icon is passed correctly', () => {
+    const wrapper = shallow(<GoogleIcon icon="sheets" />);
+    expect(wrapper.hasClass('sheets')).toEqual(true);
+  });
+
+  it('GoogleIcon. size is passed correctly', () => {
+    const wrapper = shallow(<GoogleIcon icon="sheets" size="x-small" />);
+    expect(wrapper.hasClass('x-small')).toEqual(true);
+  });
+});
