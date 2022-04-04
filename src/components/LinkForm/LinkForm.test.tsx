a3d6345dd2ed12d13ab85912630910c63361fabf
Pavel Novikov <spl.novikov@gmail.com>
2022-04-04 18:11:16 +0300

    added test file for LinkForm

---
+++ src/components/LinkForm/LinkForm.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+// Components
+import LinkForm from './LinkForm';
+// Test Data
+
+// todo: tests
+it('LinkForm is rendering', () => {
+  const wrapper = shallow(
+    <LinkForm formSubmitHandler={() => 1} abortHandler={() => 1} />,
+  );
+  expect(wrapper.childAt(0).hasClass('single-input-form')).toBe(true);
+});
