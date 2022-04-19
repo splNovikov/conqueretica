ac7e54561fc368cebd5bb2c6b8aa93ec3bef7d0c
Pavel Novikov <spl.novikov@gmail.com>
2022-04-19 14:19:38 +0300

    added firebase dev support

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-import { FIREBASE_CONFIG } from './config';
+import { FIREBASE_CONFIG_DEV, FIREBASE_CONFIG_PROD } from './config';
 import { signInWithGoogle, signOut } from './auth';
 import { createUser } from './user';
 import { getTabsQuery, addTab, updateTab, deleteTab } from './tabs';
@@ -15,7 +15,8 @@ import {
 import { addCategory, deleteCategory } from './categories';
 import { addLink, updateLink, deleteLink } from './links';
 
-const app = initializeApp(FIREBASE_CONFIG);
+const isProd = process.env.NODE_ENV === 'production';
+const app = initializeApp(isProd ? FIREBASE_CONFIG_PROD : FIREBASE_CONFIG_DEV);
 const auth = getAuth(app);
 const firestoreDB = getFirestore(app);
 
