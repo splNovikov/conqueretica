f014a0e35c3ef1607cf16e462b23535a9377b0d4
Pavel Novikov <spl.novikov@gmail.com>
2022-04-28 10:59:02 +0300

    updated tests for App Component

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { shallow } from 'enzyme';
-
+import { mount, ReactWrapper } from 'enzyme';
+import * as authHooks from 'react-firebase-hooks/auth';
+// Components
 import App from './App';
+// Test Data
+import { user } from '../../__test_data__';
+import { act } from 'react-dom/test-utils';
 
 describe('App Component', () => {
+  const appSelector = 'section.app';
+  const appHeaderSelector = 'header.app-header';
+  const appContentSelector = 'main.app-content-wrapper';
+  const appFooter = 'footer.app-footer';
+  let wrapper: ReactWrapper;
+  let app: ReactWrapper;
+  let header: ReactWrapper;
+  let content: ReactWrapper;
+  let footer: ReactWrapper;
+
+  beforeEach(async () => {
+    jest
+      .spyOn(authHooks, 'useAuthState')
+      // @ts-ignore
+      .mockImplementation(() => [user]);
+
+    await act(async () => {
+      wrapper = mount(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>,
+      );
+    });
+
+    app = wrapper.find(appSelector);
+    header = wrapper.find(appHeaderSelector);
+    content = wrapper.find(appContentSelector);
+    footer = wrapper.find(appFooter);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    wrapper.unmount();
+  });
+
   it('App is rendering', () => {
-    const wrapper = shallow(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
-    );
     expect(wrapper.exists()).toEqual(true);
+    expect(app.exists()).toEqual(true);
+    expect(header.exists()).toEqual(true);
+    expect(content.exists()).toEqual(true);
+    expect(footer.exists()).toEqual(true);
   });
 });
