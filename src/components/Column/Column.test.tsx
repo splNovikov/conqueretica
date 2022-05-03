98eee13925386e1f1e97d0b8aa5169c6cbce15ab
Pavel Novikov <spl.novikov@gmail.com>
2022-05-03 09:42:46 +0300

    Column component has been covered by tests with 100% coverage

--- src/components/Column/Column.test.tsx
+++ src/components/Column/Column.test.tsx
@@ -1,144 +1,167 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
+import * as firestoreHooks from 'react-firebase-hooks/firestore';
 // Components
 import Column from './Column';
+import Category from '../Category';
+import SingleInputForm from '../SingleInputForm';
 // Test Data
-import { columns } from '../../__test_data__';
+import { categories, columns } from '../../__test_data__';
 import { act } from 'react-dom/test-utils';
+import firebase from '../../firebase';
 
-describe('Column', () => {
-  it('Column Component is rendering', () => {
-    const wrapper = shallow(
-      <Column
-        column={columns[0]}
-        span={6}
-        deleteColumnHandler={() => 1}
-        categoryFormSubmitHandler={() => 1}
-        deleteCategoryHandler={() => 1}
-        createLinkHandler={() => 1}
-      />,
-    );
-    expect(wrapper.hasClass('column')).toEqual(true);
+describe('Column Component', () => {
+  const origConsoleError = console.error;
+  // Selectors
+  const columnHeaderSelector = 'div.column-header';
+  const enableAddCategoryButtonSelector =
+    'button.column-btn-enable-add-category';
+  // Test Data
+  const column = columns[0];
+  // Wrappers
+  let wrapper: ReactWrapper;
+  let columnHeader: ReactWrapper;
+  let categoriesWrappers: ReactWrapper<any>;
+  let enableAddCategoryButton: ReactWrapper;
+  let singleInputForm: ReactWrapper<any>;
+
+  beforeEach(() => {
+    console.error = jest.fn();
+    jest
+      .spyOn(firestoreHooks, 'useCollectionData')
+      // @ts-ignore
+      .mockImplementation(() => [categories, false, undefined]);
+    wrapper = mount(<Column column={column} span={6} />);
+    columnHeader = wrapper.find(columnHeaderSelector);
+    categoriesWrappers = wrapper.find(Category);
+    enableAddCategoryButton = wrapper.find(enableAddCategoryButtonSelector);
+    singleInputForm = wrapper.find(SingleInputForm);
   });
 
-  it('Column Component is rendering necessary elements', async () => {
-    const wrapper = mount(
-      <Column
-        column={columns[0]}
-        span={6}
-        deleteColumnHandler={() => 1}
-        categoryFormSubmitHandler={() => 1}
-        deleteCategoryHandler={() => 1}
-        createLinkHandler={() => 1}
-      />,
-    );
+  afterEach(() => {
+    jest.resetAllMocks();
+    console.error = origConsoleError;
+    wrapper.unmount();
+  });
 
-    const deleteColEl = wrapper.find('button.btn-delete-column');
-    expect(deleteColEl.exists()).toBe(true);
+  it('Column Component is rendering', () => {
+    expect(wrapper.exists()).toBe(true);
+    expect(columnHeader.exists()).toBe(true);
+    expect(categoriesWrappers.length).toBe(3);
+    expect(enableAddCategoryButton.exists()).toBe(true);
+    expect(singleInputForm.exists()).toBe(false);
+  });
 
-    const enableAddCatTrigger = wrapper.find('button.btn-enable-add-category');
-    expect(enableAddCatTrigger.exists()).toBe(true);
+  it('Column Component is handling errors when categories are not loading', () => {
+    jest.resetAllMocks();
+    jest
+      .spyOn(firestoreHooks, 'useCollectionData')
+      // @ts-ignore
+      .mockImplementation(() => [[], false, { message: 'err' }]);
 
-    await act(async () => {
-      enableAddCatTrigger.simulate('click');
+    wrapper.unmount();
+    wrapper = mount(<Column column={column} span={6} />);
+
+    expect(console.error).toHaveBeenCalledWith({ message: 'err' });
+  });
+
+  describe('Column Component Interactions', () => {
+    it('Should enable Create Category Mode', async () => {
+      await act(async () => {
+        enableAddCategoryButton.simulate('click');
+      });
+
+      wrapper.update();
+
+      expect(wrapper.find(SingleInputForm).exists()).toBe(true);
+    });
+  });
+
+  describe('Column Component -> SingleInputForm Component Handlers', () => {
+    it('Should invoke "Add Category Handler"', async () => {
+      firebase.addCategory = jest.fn();
+
+      await act(async () => {
+        enableAddCategoryButton.simulate('click');
+      });
+
+      wrapper.update();
+
+      const formSubmitHandler = wrapper
+        .find(SingleInputForm)
+        .prop('formSubmitHandler');
+
+      formSubmitHandler && formSubmitHandler('category_title');
+
+      wrapper.update();
+
+      expect(firebase.addCategory).toHaveBeenCalledWith(
+        'category_title',
+        column,
+      );
+      expect(wrapper.find(SingleInputForm).exists()).toBe(false);
     });
 
-    wrapper.update();
+    it('Should invoke Abort "Add Category Handler"', async () => {
+      await act(async () => {
+        enableAddCategoryButton.simulate('click');
+      });
 
-    const addFormEl = wrapper.find('form.single-input-form');
-    expect(addFormEl.exists()).toBe(true);
+      wrapper.update();
 
-    const categoriesEl = wrapper.find('Category');
-    expect(categoriesEl.length).toBe(2);
+      const abortHandler = wrapper.find(SingleInputForm).prop('abortHandler');
+
+      abortHandler && abortHandler();
+
+      wrapper.update();
+
+      expect(wrapper.find(SingleInputForm).exists()).toBe(false);
+    });
   });
 
-  it('Column Component "Delete Column" button should invoke "Delete Column" method', () => {
-    const handleDeleteColumn = jest.fn();
-    const wrapper = mount(
-      <Column
-        column={columns[0]}
-        span={6}
-        deleteColumnHandler={handleDeleteColumn}
-        categoryFormSubmitHandler={() => 1}
-        deleteCategoryHandler={() => 1}
-        createLinkHandler={() => 1}
-      />,
-    );
+  describe('Column Component -> Category Component Handlers', () => {
+    it('Should invoke "Delete Category Handler" and delete Column as well', async () => {
+      const category = categories[0];
+      jest.resetAllMocks();
+      jest
+        .spyOn(firestoreHooks, 'useCollectionData')
+        // @ts-ignore
+        .mockImplementation(() => [[category], false, undefined]);
 
-    // modal is closed
-    const modalEl = wrapper.find('.ant-modal-root');
-    expect(modalEl.exists()).toBe(false);
+      wrapper.unmount();
+      wrapper = mount(<Column column={column} span={6} />);
 
-    // trigger delete icon to show modal
-    const deleteColEl = wrapper.find('button.btn-delete-column');
-    deleteColEl.simulate('click');
+      firebase.deleteCategoryWithColumnScenario = jest.fn();
+      firebase.deleteCategory = jest.fn();
 
-    // modal appeared
-    const modalEl2 = wrapper.find('.ant-modal-root .ant-modal-wrap');
-    expect(modalEl2.exists()).toBe(true);
-    expect(modalEl2.prop('style')).not.toHaveProperty('display', 'none');
+      const categoryWrapper = wrapper.find(Category);
+      const deleteCategoryHandler = categoryWrapper.prop(
+        'deleteCategoryHandler',
+      );
 
-    // modal contains OK button
-    const modalFooterEl = modalEl2.find('.ant-modal-footer');
-    const confirmBtnEl = modalFooterEl.findWhere(
-      (node) => node.type() === 'button' && node.text() === 'OK',
-    );
-    expect(confirmBtnEl.exists()).toBeTruthy();
+      deleteCategoryHandler && deleteCategoryHandler(category);
 
-    // click OK button
-    confirmBtnEl.simulate('click');
+      expect(firebase.deleteCategoryWithColumnScenario).toHaveBeenCalledWith(
+        category,
+        column,
+      );
+      expect(firebase.deleteCategory).not.toHaveBeenCalled();
+    });
 
-    // expect calling delete function
-    expect(handleDeleteColumn).toHaveBeenCalledWith(columns[0]);
+    it('Should invoke "Delete Category Handler"', async () => {
+      firebase.deleteCategoryWithColumnScenario = jest.fn();
+      firebase.deleteCategory = jest.fn();
 
-    // modal is closed
-    const modalEl3 = wrapper.find('.ant-modal-root .ant-modal-wrap');
-    expect(modalEl3.exists()).toBe(true);
-    expect(modalEl3.prop('style')).toHaveProperty('display', 'none');
-  });
+      const category = categories[0];
+      const categoryWrapper = categoriesWrappers.at(0);
+      const deleteCategoryHandler = categoryWrapper.prop(
+        'deleteCategoryHandler',
+      );
 
-  it('Column Component - There is an ability to hide Confirmation Modal by clicking on Cancel Button', () => {
-    const handleDeleteColumn = jest.fn();
-    const wrapper = mount(
-      <Column
-        column={columns[0]}
-        span={6}
-        deleteColumnHandler={handleDeleteColumn}
-        categoryFormSubmitHandler={() => 1}
-        deleteCategoryHandler={() => 1}
-        createLinkHandler={() => 1}
-      />,
-    );
+      deleteCategoryHandler && deleteCategoryHandler(category);
 
-    // modal is closed
-    const modalEl = wrapper.find('.ant-modal-root');
-    expect(modalEl.exists()).toBe(false);
-
-    // trigger delete icon to show modal
-    const deleteColEl = wrapper.find('button.btn-delete-column');
-    deleteColEl.simulate('click');
-
-    // modal appeared
-    const modalEl2 = wrapper.find('.ant-modal-root .ant-modal-wrap');
-    expect(modalEl2.exists()).toBe(true);
-    expect(modalEl2.prop('style')).not.toHaveProperty('display', 'none');
-
-    // modal contains OK button
-    const modalFooterEl = modalEl2.find('.ant-modal-footer');
-    const cancelBtnEl = modalFooterEl.findWhere(
-      (node) => node.type() === 'button' && node.text() === 'Cancel',
-    );
-    expect(cancelBtnEl.exists()).toBeTruthy();
-
-    // click OK button
-    cancelBtnEl.simulate('click');
-
-    // expect NOT calling delete function
-    expect(handleDeleteColumn).toHaveBeenCalledTimes(0);
-
-    // modal is closed
-    const modalEl3 = wrapper.find('.ant-modal-root .ant-modal-wrap');
-    expect(modalEl3.exists()).toBe(true);
-    expect(modalEl3.prop('style')).toHaveProperty('display', 'none');
+      expect(firebase.deleteCategoryWithColumnScenario).not.toHaveBeenCalled();
+      expect(firebase.deleteCategory).toHaveBeenCalledWith(category);
+    });
   });
 });
