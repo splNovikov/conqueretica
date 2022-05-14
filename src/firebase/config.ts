b8eaf81b336009b363454cc6895d062d061605ff
Pavel Novikov <spl.novikov@gmail.com>
2022-05-14 07:54:09 +0300

    updated config to pass coverage

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,20 +1,20 @@
 // todo: this is a temporary solution - store it somewhere
 const FIREBASE_CONFIG_PROD = {
-  apiKey: '',
+  apiKey: 'dummy-prod-api-key',
   authDomain: 'conqueretica.firebaseapp.com',
   projectId: 'conqueretica',
   storageBucket: 'conqueretica.appspot.com',
-  messagingSenderId: '',
-  appId: '',
+  messagingSenderId: 'dummy-prod-messaging-sender-id',
+  appId: 'dummy-prod-app-id',
 };
 
 const FIREBASE_CONFIG_DEV = {
-  apiKey: '',
+  apiKey: 'dummy-dev-api-key',
   authDomain: 'conqueretica-dev.firebaseapp.com',
   projectId: 'conqueretica-dev',
   storageBucket: 'conqueretica-dev.appspot.com',
-  messagingSenderId: '',
-  appId: '',
+  messagingSenderId: 'dummy-dev-messaging-sender-id',
+  appId: 'dummy-dev-app-id',
 };
 
 export const getFirebaseConfig = (env: string) => {
