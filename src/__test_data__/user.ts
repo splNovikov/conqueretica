ade7f7e0e4b500b67da3cd9ca19ed9057891311e
Pavel Novikov <spl.novikov@gmail.com>
2022-05-20 20:34:13 +0300

    updated user test data - added correct url

--- src/__test_data__/user.ts
+++ src/__test_data__/user.ts
@@ -4,7 +4,8 @@ export const user: UserInfo = {
   displayName: 'Pavel Novikov',
   email: 'email@email.com',
   phoneNumber: 'phone',
-  photoURL: '',
+  photoURL:
+    'https://lh3.googleusercontent.com/a-/AOh14GjASSAlbBrAV8jQmMLt8B-KZ48y6lJ3nnhYOumiFVM=s96-c',
   providerId: '',
   uid: 'user_uid',
 };
