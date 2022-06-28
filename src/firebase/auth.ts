07ea7d053497996b3310d874cef07243cf7e27bd
Pavel Novikov <spl.novikov@gmail.com>
2022-06-28 20:28:06 +0300

    removed chrome authentication and types

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,60 +1,11 @@
-import {
-  UserInfo,
-  GoogleAuthProvider,
-  signInWithCredential,
-} from 'firebase/auth';
-import { getDoc, doc } from 'firebase/firestore';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 import firebase from './index';
 
-// todo: tests
-const getToken = (userInfo: chrome.identity.UserInfo): Promise<string> => {
-  return new Promise((resolve) => {
-    chrome.identity.getAuthToken(
-      { account: { id: userInfo.id }, interactive: true },
-      (token: string) => {
-        resolve(token);
-      },
-    );
-  });
+const signIn = (email: string, password: string) => {
+  return signInWithEmailAndPassword(firebase.auth, email, password);
 };
 
-// todo: tests
-const getProfileUserInfo = (): Promise<chrome.identity.UserInfo> => {
-  return new Promise((resolve) => {
-    chrome.identity.getProfileUserInfo(
-      { accountStatus: chrome.identity.AccountStatus.ANY },
-      (userInfo) => {
-        resolve(userInfo);
-      },
-    );
-  });
-};
+const signOut = () => firebase.auth.signOut();
 
-// todo: update tests
-const signInWithGoogle = async (): Promise<UserInfo | null> => {
-  const profile = await getProfileUserInfo();
-
-  const token = await getToken(profile);
-
-  const credential = GoogleAuthProvider.credential(null, token);
-
-  const { user } = await signInWithCredential(firebase.auth, credential);
-
-  // todo: move to getUser
-  const userRef = doc(firebase.firestoreDB, 'users', user.uid);
-
-  const userDoc = await getDoc(userRef);
-
-  if (!userDoc.exists()) {
-    return firebase.createUser(user);
-  }
-
-  return user;
-};
-
-const signOut = () => {
-  firebase.auth.signOut();
-};
-
-export { signInWithGoogle, signOut };
+export { signIn, signOut };
