2c50594544777d4485d7527eee78f2bf72dad4ed
Pavel Novikov <spl.novikov@gmail.com>
2022-06-28 20:04:32 +0300

    updated firebase's createUser

--- src/firebase/user.ts
+++ src/firebase/user.ts
@@ -1,26 +1,20 @@
-import { UserInfo } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, UserCredential } from 'firebase/auth';
 // Firebase
 import firebase from './index';
 // Utils
 import { defaultErrorHandler, httpErrorHandler } from '../utils';
 
-const createUser = async (user: UserInfo): Promise<UserInfo | null> => {
-  if (!user?.uid) {
-    defaultErrorHandler('No User');
+const createUser = async (
+  email: string,
+  password: string,
+): Promise<UserCredential | null> => {
+  if (!email || !password) {
+    defaultErrorHandler('No email or Password');
     return null;
   }
 
   try {
-    const userRef = doc(firebase.firestoreDB, 'users', user.uid);
-
-    await setDoc(userRef, {
-      uid: user.uid,
-      name: user.displayName,
-      authProvider: 'google',
-      email: user.email,
-    });
-    return user;
+    return await createUserWithEmailAndPassword(firebase.auth, email, password);
   } catch (e) {
     httpErrorHandler(e);
     return null;
