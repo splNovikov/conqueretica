5cf732f2d513dacbb01cd995c156fb5fd99319d1
Pavel Novikov <spl.novikov@gmail.com>
2022-06-28 20:31:19 +0300

    updated SignIn and SignUp pages

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 // Firebase
+// Context
+import { UserAuth } from '../../context/authContext';
 // Interfaces
 // Components
 // Utils
 
 const SignInPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { signIn } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setError('');
+    try {
+      await signIn(email, password);
+      navigate('/links');
+    } catch (err: any) {
+      setError(err.message);
+      console.log(err.message);
+    }
+  };
+
   return (
     <div>
       <h1>Sign in to your account</h1>
@@ -13,17 +33,21 @@ const SignInPage = () => {
         Don&apos;t have an account yet? <Link to="/signup">Sign Up</Link>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <span>Email Address</span>
-          <input type="email" />
+          <input type="email" onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div>
           <span>Password</span>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button type="submit">Sign In</button>
       </form>
+      <div>{error}</div>
     </div>
   );
 };
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 // Firebase
+// Context
+import { UserAuth } from '../../context/authContext';
 // Interfaces
 // Components
 // Utils
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { createUser } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setError('');
+
+    try {
+      await createUser(email, password);
+      navigate('/links');
+    } catch (err: any) {
+      setError(err.message);
+      console.log(err.message);
+    }
+  };
+
   return (
     <div>
       <h1>Sign up for a free</h1>
@@ -13,17 +34,21 @@ const SignUpPage = () => {
         Already have an account? <Link to="/signin">Sign in</Link>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <span>Email Address</span>
-          <input type="email" />
+          <input type="email" onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div>
           <span>Password</span>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button type="submit">Sign Up</button>
       </form>
+      <div>{error}</div>
     </div>
   );
 };
